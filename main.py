1ba061c811409f97783fb1c761f9e2118e626286
Denis Polyakov <p-ov.da@ya.ru>
2022-04-11 16:18:33 +0300

    edit

--- main.py
+++ main.py
@@ -3,73 +3,68 @@ import urllib.parse
 
 import requests
 from dotenv import load_dotenv
+from requests.exceptions import ConnectionError, MissingSchema, HTTPError
 
-load_dotenv()
-
-BITLINKS_TOKEN = os.getenv("TOKEN")
+BITLINK_API_URL = 'https://api-ssl.bitly.com/v4/bitlinks/'
 
 
-def shorten_link(token: str, url: str) -> str:
+def shorten_link(entered_long_url: str, header: dict) -> str:
     """From long ling to short link"""
-    url_bitlinks = 'https://api-ssl.bitly.com/v4/bitlinks'
-    headers = {
-        'Authorization': f'Bearer {token}',
-    }
+    url_bitlinks = f'{BITLINK_API_URL}'
     payload = {
-        "long_url": url
+        "long_url": entered_long_url
     }
-    response = requests.post(url_bitlinks, headers=headers, json=payload)
+    response = requests.post(url_bitlinks, headers=header, json=payload)
     response.raise_for_status()
     return response.json()['id']
 
 
-def parse_link(url: str) -> str:
+def parse_link(long_url: str) -> str:
     """Return netloc + path from url"""
-    parsed = urllib.parse.urlparse(url)
+    parsed = urllib.parse.urlparse(long_url)
     return parsed.netloc + parsed.path
 
 
-def count_clicks(token: str, bitlink: str, unit: str = 'day', units: int = -1) -> int:
+def count_clicks(entered_bitlink: str, header: dict, unit: str = 'day', units: int = -1) -> int:
     """Count clicks on bitlink"""
-    url_clicks_summary = f'https://api-ssl.bitly.com/v4/bitlinks/{parse_link(bitlink)}/clicks/summary'
-    headers = {
-        'Authorization': f'Bearer {token}',
-    }
+    url_clicks_summary = f'{BITLINK_API_URL}{parse_link(entered_bitlink)}/clicks/summary'
     payload = {
         "unit": unit,
         "units": units,
     }
-    response = requests.get(url_clicks_summary, headers=headers, params=payload)
+    response = requests.get(url_clicks_summary, headers=header, params=payload)
     response.raise_for_status()
-    clicks_count = response.json()['total_clicks']
-    return clicks_count
+    return response.json()['total_clicks']
 
 
-def is_bitlink(bitlink):
+def is_bitlink(entered_link: str, header: dict) -> bool:
     """Check url for bitlink"""
-    check_bitlink_url = f'https://api-ssl.bitly.com/v4/bitlinks/{parse_link(bitlink)}'
-    headers = {
-        'Authorization': f'Bearer {BITLINKS_TOKEN}',
-    }
-    response = requests.get(check_bitlink_url, headers=headers)
+    check_bitlink_url = f'{BITLINK_API_URL}{parse_link(entered_link)}'
+    response = requests.get(check_bitlink_url, headers=header)
     return response.ok
 
 
 def main():
     """Start program"""
-    link = input('Введите ссылку: ')
+    load_dotenv()
+    bitlink_token = os.getenv("TOKEN")
+    bitlink_header = {'Authorization': f'Bearer {bitlink_token}'}
+    entered_link = input('Введите ссылку: ')
     try:
-        response = requests.get(link)
+        response = requests.get(entered_link)
         response.raise_for_status()
-        if is_bitlink(link):
-            clicks_count = count_clicks(BITLINKS_TOKEN, link)
+        if is_bitlink(entered_link, bitlink_header):
+            clicks_count = count_clicks(entered_link, bitlink_header)
             print('Количество кликов:', clicks_count)
         else:
-            bitlink_result = shorten_link(BITLINKS_TOKEN, link)
+            bitlink_result = shorten_link(entered_link, bitlink_header)
             print('Битлинк', bitlink_result)
-    except (requests.exceptions.ConnectionError, requests.exceptions.MissingSchema, requests.exceptions.HTTPError):
+    except (ConnectionError, MissingSchema, HTTPError):
         print('Incorrect url')
 
 
 if __name__ == '__main__':
     main()
+
+# https://bit.ly/3JhwFuG
+# https://github.com/
